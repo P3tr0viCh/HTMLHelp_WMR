d6344faa44df358052c6f30511181f1e2ccacc9f
P3tr0viCh <P3tr0viCh@mail.ru>
2017-02-05 15:18:59 +0500

    Initial commit.

---
+++ a_alias.h
@@ -0,0 +1,26 @@
+IDH_USING = Using.htm
+
+IDH_LOGIN = Using.htm#P_2_1
+IDH_MAIN = Using.htm#P_2_2
+IDH_MANUAL = Using.htm#P_2_3
+IDH_AUTO = Using.htm#P_2_4
+IDH_VESCOM = Using.htm#P_2_5
+IDH_EDIT = Using.htm#P_2_6
+IDH_TARE = Using.htm#P_2_7
+IDH_TRAINS = Using.htm#P_2_8
+IDH_TARES = Using.htm#P_2_9
+IDH_BAKES = Using.htm#P_2_10
+
+IDH_OPTIONS = Options.htm
+IDH_OPTIONS_PROG = Options.htm#P_4_1
+IDH_OPTIONS_SCALES = Options.htm#P_4_2
+IDH_OPTIONS_MYSQL = Options.htm#P_4_3
+IDH_OPTIONS_MYSQL_OPTIONS = Options.htm#P_4_4
+IDH_OPTIONS_AVITEK = Options.htm#P_4_5
+IDH_OPTIONS_BAKES = Options.htm#P_4_6
+IDH_OPTIONS_VANTYPES = Options.htm#P_4_7
+IDH_OPTIONS_CARGOTYPES = Options.htm#P_4_8
+IDH_OPTIONS_TERMINALS = Options.htm#P_4_9
+IDH_OPTIONS_VANMODES = Options.htm#P_4_10
+IDH_OPTIONS_USERS = Options.htm#P_4_11
+IDH_OPTIONS_RECEIVERS = Options.htm#P_4_12
